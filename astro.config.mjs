2352616f171b6baf5b8dca24cedeeec85ac7339d
ThatOneCalculator <kainoa@t1c.dev>
2023-12-12 12:18:31 -0800

    QoL fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import partytown from "@astrojs/partytown";
 
-import icon from "astro-icon"
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    optimizeHoistedScript: true,
-  },
-
+  site: "https://www.belief.gg",
   integrations: [
     icon({
       include: {
-        ph: ["hand-pointing-bold", "link-bold", "confetti-bold", "hand-fist-bold", "cursor-text-bold", "arrow-left-bold", "envelope-open-bold"],
-      },
+        ph: ["hand-pointing-bold", "link-bold", "confetti-bold", "hand-fist-bold", "cursor-text-bold", "arrow-left-bold", "envelope-open-bold"]
+      }
     }),
-  ],
+    partytown(),
+  ]
 });
