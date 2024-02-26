818ec93bcc38ea2d999bfea5a4a0d0c2ee1a0213
ThatOneCalculator <kainoa@t1c.dev>
2024-02-25 22:01:47 -0800

    FINALLY FIX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import partytown from "@astrojs/partytown";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://inertia.social",
+  // site: "https://www.inertia.social",
   integrations: [
     icon({
       include: {
         ph: ["*"]
       }
     }),
-    // partytown(),
   ]
 });
